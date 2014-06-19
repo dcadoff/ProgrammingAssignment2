6f54ae4540d10ab896f88a25085f426ef1dceaa7
dcadoff <dave.cadoff@gmail.com>
2014-06-19 12:06:22 -0700

    Added comments about script and functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script contains two functions, makeCacheMatrix and cacheSolve, that when
+## used together, will accept any invertible, square matrix and solve for the inverse
+## matrix, display the results and cache the inverse in memory. The caching is
+## designed to save processing time by preventing a matrix that's the same as
+## the previously submitted matrix from being recalculated. 
 
-## Write a short comment describing this function
-## 6.15.2014 - Nothing to see here. No changes today. Just testing ability to push back to Github.
+## makeCacheMatrix creates a list of four (4) functions:
+## The functions are:
+## set - which sets the value of a matrix
+## get - gets the value of a matrix
+## setinverse - sets the inverse value of a matrix
+## getinverse - gets the inverse value of a matrix
+## These functions are designed to be called from other functions; hence lexical
+## scoping is needed in the makeCacheMatrix function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve does two things:
+## First, it checks to see if the inverse of a square matrix has been cached in memory.
+## Then, if the inverse has been cached, the solve is displayed.
+## If the square matrix is new, then the function solves for the inverse, displays
+## the new results and, finally, caches the new inverse in memory.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
